4eb36162315504b7720bb16051e5bccadb87448d
yakuza-B <barryng1119@gmail.com>
2025-04-08 20:57:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import joblib
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pdfplumber
